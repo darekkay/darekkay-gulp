49cd6abe1044d2e2a966627b366898052dcc8eb0
Darek Kay <hello@darekkay.com>
2020-12-12 17:55:39 +0100

    Simplify task usage

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 const gulp = require("gulp");
 
+const tasks = (config) => ({
+  assets: require("./tasks/assets")(config),
+  clean: require("./tasks/clean")(config),
+  content: require("./tasks/content")(config),
+  env: require("./tasks/env")(config),
+  purge: require("./tasks/purge")(config),
+  scripts: require("./tasks/scripts")(config),
+  styles: require("./tasks/styles")(config),
+});
+
 // re-export gulp
-module.exports = gulp;
+module.exports = {
+  gulp,
+  tasks,
+};
