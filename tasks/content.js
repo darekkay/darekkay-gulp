2e101556d0ceda6986d9ff7ee24c4fb9ef8a770e
Darek Kay <hello@darekkay.com>
2020-12-12 17:01:33 +0100

    Minify inline CSS and JS

--- tasks/content.js
+++ tasks/content.js
@@ -9,7 +9,14 @@ const htmlmin = require("gulp-htmlmin");
 module.exports = ({ paths }) => {
   const contentTask = () => {
     return src(paths.content)
-      .pipe(htmlmin({ collapseWhitespace: true, removeComments: true }))
+      .pipe(
+        htmlmin({
+          collapseWhitespace: true,
+          removeComments: true,
+          minifyCSS: true,
+          minifyJS: true, // TODO: ES6 is not minified: https://github.com/samvloeberghs/kwerri-oss/pull/43
+        })
+      )
       .pipe(dest(paths.destination));
   };
   contentTask.displayName = "content";
