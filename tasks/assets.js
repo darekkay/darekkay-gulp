e270c9000cd9911640d7b3d66fabd52ad41f1afd
Darek Kay <hello@darekkay.com>
2020-12-19 09:43:03 +0100

    Allow defining multiple asset paths

--- tasks/assets.js
+++ tasks/assets.js
@@ -1,10 +1,18 @@
 const { src, dest } = require("gulp");
 
 /* Passthrough assets */
+const copy = ({ source, destination }) => {
+  return src(`${source}/**/*`).pipe(dest(destination));
+};
+
 module.exports = ({ paths }) => {
   const assetsTask = () => {
-    return src(`${paths.assets.source}/**/*`).pipe(
-      dest(paths.assets.destination)
+    const assets = Array.isArray(paths.assets) ? paths.assets : [paths.assets]; // support arrays or objects
+
+    return Promise.all(
+      assets.map((path) =>
+        copy({ source: path.source, destination: path.destination })
+      )
     );
   };
   assetsTask.displayName = "assets";
