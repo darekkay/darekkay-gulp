d62765da522aa790960802e5cfd2ac6f40c807e6
Darek Kay <hello@darekkay.com>
2021-03-29 10:51:04 +0200

    Add watcher task name

--- index.js
+++ index.js
@@ -12,13 +12,16 @@ const tasks = (config) => {
     styles: config.paths.styles && require("./tasks/styles")(config),
     icons: config.paths.icons && require("./tasks/icons")(config),
   };
-  enabledTasks.watch = (...tasksToWatch) => () => {
-    tasksToWatch.forEach((taskToWatch) => {
-      // we pass explicit tasks to watch to prevent watching for tasks that are not used (e.g. "content" being only used in production, but not development)
-      if (taskToWatch.watcher) taskToWatch.watcher();
-    });
+  enabledTasks.watch = (...tasksToWatch) => {
+    const watcher = () => {
+      tasksToWatch.forEach((taskToWatch) => {
+        // we pass explicit tasks to watch to prevent watching for tasks that are not used (e.g. "content" being only used in production, but not development)
+        if (taskToWatch.watcher) taskToWatch.watcher();
+      });
+    };
+    watcher.displayName = "watch";
+    return watcher;
   };
-  enabledTasks.watch.displayName = "watch";
   return enabledTasks;
 };
 
